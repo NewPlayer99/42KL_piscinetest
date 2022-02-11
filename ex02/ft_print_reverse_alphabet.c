fa0de0c3e5a8e574d99c6b1e30fde064a2c3bf29
Wan <sayabukanwan@gmail.com>
2022-02-11 23:05:56 +0800

    initial commit

---
+++ ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+void ft_print_reverse_alphabet(void)
+{
+	for(char c = 'z'; c >= 'a'; c--)
+	{
+		write(1, &c, 1);
+	}
+}
+
+int main(void)
+{
+	ft_print_reverse_alphabet();
+	write(1, "\n", 1);
+	return 0;
+}
